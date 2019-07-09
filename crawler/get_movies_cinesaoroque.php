8cfef8ccec71cbfeb915af39801133a149826a4f
Vinicius Souza <viisouza10@live.com>
2019-07-09 17:50:35 -0300

    return string get movies :(

--- crawler/get_movies_cinesaoroque.php
+++ crawler/get_movies_cinesaoroque.php
@@ -4,6 +4,7 @@
 function tirarAcentos($string){
     $string =  preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
     $string = str_replace(":","",$string);
+    return $string;
 }
 
 include('simple_html_dom.php');
